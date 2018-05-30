fd7d5d81d09abe8668734442f703b73389f2a104
Vajay Attila <vajay.attila@gmail.com>
2018-05-30 16:03:03 +0200

    Add simple field validation functions
    add getfieldvalue
    add fieldisexist

--- application/extensions/restserver.php
+++ application/extensions/restserver.php
@@ -12,6 +12,9 @@ if (! defined ( 'mutyurphpmvc_inited' ))
  *  @version 1.0.0.0
  */
 
+ /**
+  *  @brief Mütyür RestFul server base class 
+  */
 class restserver extends helper{
 	protected $method;
 	protected $action;
@@ -66,7 +69,13 @@ class restserver extends helper{
 					(strpos(strtolower($this->get_content_type()),'text/plain')!==false) ||
 					(strpos(strtolower($this->get_content_type()),'application/json')!==false)
 				){
+					$this->log_message('debug', print_r(json_decode(file_get_contents("php://input")),true));					
 					$postdata=json_decode(file_get_contents("php://input"), true);
+					if($postdata===NULL){
+						$jsonerror=json_last_error_msg();
+						echo "JSON decode status: $jsonerror";
+						exit();
+					}
 				} else {
 					$postdata=$_POST;
 				}
@@ -158,8 +167,64 @@ class restserver extends helper{
 		$this->response($vers);
 	}
 
+	/**
+	 * @brief Register a callable method
+	 */
 	public function addfunction($method/*'GET','POST','PUT','DELETE'*/, $name){
 		$this->registredActions[$method][]=$name;
+	}
+
+	/**
+	 * @brief Get a filed value by type and filters
+	 * @param $name the name of filed name or array index
+	 * @param $required the field is required or not
+	 * @param $regexpattern only for 'field' type
+	 * @param $node if $node is NULL then $node=$this->requestArgs
+	 */
+	public function getfieldvalue($nameorindex, $required=FALSE, $regexpattern=NULL, $node=NULL){
+		$return=FALSE;
+		if($node===NULL){
+			$node=$this->requestArgs;
+		}
+		if($this->fieldisexist($nameorindex, $node)){
+			// check field type
+			$value=$node[$nameorindex];
+			if(is_object($value)){
+				$return=$value; // return the object
+			} else if (is_array($value)){
+				// is associative
+				$return=$value; // return array
+			} else {
+				if($regexpattern!==NULL){
+					$subject = "abcdef";
+					$pattern = '/'.$regexpattern.'/';
+					$regexret=preg_match($pattern, $value, $matches, PREG_OFFSET_CAPTURE);
+					if($regexret===1){
+						$return=$value;
+					} else if($regexret===0){
+						echo "Pattern not match in '$nameorindex' field. The value is '$value'. The pattern is '$pattern'\n";	
+					} else {
+						echo "Regex error. Parameter name is '$nameorindex'. The value is '$value'. The pattern is '$pattern'\n";
+					}	
+				} else {
+					$return=$value;
+				}
+			}
+		} else {
+			if($required===TRUE){
+				echo "Missing field: $nameorindex\n";
+			}else{
+				$return=NULL;
+			}
+		}
+		return $return;
+	}
+
+	public function fieldisexist($name, $node=NULL){
+		if($node===NULL){
+			$node=$this->requestArgs;
+		}
+		return isset($node[$name]);		
 	}
 
 }
